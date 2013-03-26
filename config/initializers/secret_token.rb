7501a8ed71ff25a97adf09b36ed733fe07d6b4bd
Joshua Petitt <joshpetitt@jpmec.com>
2013-03-25 20:05:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listlust::Application.config.secret_token = 'b5ff61625ba5dafdf9b886cd0d66166fed4b060f4acf7bc690974f34c2ad8044df98b61958a30ade6ed642aa6041f133df9beeeeacae109d079e5d381367b3bb'
